54924f024f4a97688a5c7e3890c6f808be3243e3
Gary McGovern <gazzaplays@gmail.com>
2020-04-11 03:07:29 +0000

    Added django models for plan type and subscribers. Displayed this in admin panel with several fields and ordering options, search fields needs to be fixed.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import SubPlan, Subscriber
 
-# Register your models here.
+""" https://stackoverflow.com/questions/48665353/django-1-11-admin-list-filter-to-include-fields-in-another-model """
+""" https://djangobook.com/mdj2-django-admin/ """
+
+
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'subscription_date', 'subscription_end_date')
+    ordering = ('user',)
+    search_fields = ('user', 'plan')
+
+
+admin.site.register(SubPlan)
+admin.site.register(Subscriber, SubscriberAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SubPlan(models.Model):
+    plan_name = models.CharField(max_length=50)
+    plan_price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.plan_name
+
+
+class Subscriber(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plan = models.ForeignKey(SubPlan, on_delete=models.CASCADE)
+    subscription_date = models.DateField()
+    subscription_end_date = models.DateField()
+
+    def __str__(self):
+        return self.user.email
