b4fc688d1645b2c6f82e21960adbf8aa21570b83
Gary McGovern <gazzaplays@gmail.com>
2020-04-13 05:37:09 +0000

    Fixed module name in accounts.views file for content

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Subscriber
 from accounts.forms import UserLoginForm, UserRegistrationForm
-from content.views import home_page
+from content.views import content_view
 
 
 def index(request):
