5ac56b80097eaa971b674634d0beca33b2c607f9
Gary McGovern <gazzaplays@gmail.com>
2020-04-14 00:35:24 +0000

    Improved column controls on home page. Updated css to work across multiple browsers

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
+    /* Hides/Shows Controls when mouse hovers over column */
     $(".column").hover( showControls, hideControls);
 
+    /* Moves Items when clicking arrow buttons */
     $(".fas").click(function(){
         let id = $(this).parent().attr('id')
         let curScroll = $('#' + id + ".column").scrollLeft();
-        console.log(id);
 
         if($(this).hasClass("fa-angle-left")){
             $('#' + id + ".column").scrollLeft(curScroll - 50);
@@ -14,13 +15,45 @@ $( document ).ready(function() {
     });
 });
 
-function showControls(){
-    let column = $(this).attr("id");
-    /* show Arrows */
-    $('#' + column + ' .fas.fa-angle-left').show();
-    $('#' + column + ' .fas.fa-angle-right').show();
+/**
+ * 
+ * @param {jQuery Object} item 
+ * Checks if item is is visible om screen
+ */
+function isVisible(item){
+    let result = false;
+    let offset = item.offset();
+
+    if(offset.left < window.innerWidth){
+        result = true;
+    }
+    return result;
 }
 
+/**
+ * Shows controls only when there is items
+ * hidden.
+ */
+function showControls(){
+    let id = $(this).attr("id");
+    let curScroll = $("#" + id + ".column").scrollLeft();
+
+    $("#" + id + ".column img").each(function(item){
+        if(!isVisible($(this))){
+            /* show Arrows */
+            if(curScroll > 0){
+                $('#' + id + ' .fas.fa-angle-left').show();
+                $('#' + id + ' .fas.fa-angle-right').show();
+            } else {
+                $('#' + id + ' .fas.fa-angle-right').show();
+            }
+        } 
+    });
+}
+
+/**
+ * Hides controls.
+ */
 function hideControls(){
     /* hide Arrows */
     $('.fas.fa-angle-left').hide();
