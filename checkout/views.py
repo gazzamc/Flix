3e472fb7c84d25a2a851baf1fa6b0f688fe31e2e
Gary McGovern <gazzaplays@gmail.com>
2020-04-16 14:16:30 +0000

    Styled plan page and added form to forward plan type to payment page. Added check in content view function to see if user was subscribed

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from accounts.models import SubPlan, Subscriber
 
 @login_required
 def plans(request):
     """Subscription plans page"""
-    return render(request, 'plans.html')
+    plans = SubPlan.objects.all()
+
+    """ Check if user is sub already """
+
+    try:
+        subscriber = Subscriber.objects.get(user=request.user.id)
+    except Subscriber.DoesNotExist:
+        subscriber = None
+
+    if subscriber:
+        user_plan = subscriber.plan
+    else:
+        user_plan = None
+
+    context = {
+        'plans': plans,
+        'curr_plan': user_plan
+    }
+
+    return render(request, 'plans.html', context)
 
 @login_required
 def payment(request):
     """Subscription plans page"""
-    return render(request, 'checkout.html')
+    plan_type = request.POST.get('plan_type')
+
+    return render(request, 'checkout.html', {'plan': plan_type})
--- content/views.py
+++ content/views.py
@@ -9,35 +9,38 @@ from itertools import chain
 @login_required
 def content_view(request):
     """ Display all video content by genre"""
+    subscriber = Subscriber.objects.filter(user=request.user.id)
+    if subscriber:
+        """ Get Featured Content """
+        featured_vid = Video.objects.get(featured=True)
+        categories = Categorie.objects.all()
+        all_videos = Video.objects.none()
+        final_video_list = []
 
-    """ Get Featured Content """
-    featured_vid = Video.objects.get(featured=True)
-    categories = Categorie.objects.all()
-    all_videos = Video.objects.none()
-    final_video_list = []
+        """ Get 20 videos from each category and combine queryset """
+        for category in categories:
 
-    """ Get 20 videos from each category and combine queryset """
-    for category in categories:
+            videos = Video.objects.filter(category=category)[:2]
 
-        videos = Video.objects.filter(category=category)[:2]
+            if all_videos is None:
+                all_videos = videos
+            else:
+                """ https://stackoverflow.com/questions/38967599/joining-two-querysets-in-django """
+                final_video_list = list(chain(videos, final_video_list))
 
-        if all_videos is None:
-            all_videos = videos
-        else:
-            """ https://stackoverflow.com/questions/38967599/joining-two-querysets-in-django """
-            final_video_list = list(chain(videos, final_video_list))
+        content = {
+            "videos": final_video_list,
+            "categories": categories,
+            "video_url": get_video_url(featured_vid.youtube_link),
+            "video_title": featured_vid.title,
+            "video_desc": featured_vid.description,
+            "video_img": featured_vid.image_landscape,
+            "video_link": featured_vid.slug,
+        }
 
-    content = {
-        "videos": final_video_list,
-        "categories": categories,
-        "video_url": get_video_url(featured_vid.youtube_link),
-        "video_title": featured_vid.title,
-        "video_desc": featured_vid.description,
-        "video_img": featured_vid.image_landscape,
-        "video_link": featured_vid.slug,
-    }
-
-    return render(request, 'home.html', content)
+        return render(request, 'home.html', content)
+    else:
+        return redirect(reverse('plans'))
 
 @login_required
 def video_view(request, slug):
